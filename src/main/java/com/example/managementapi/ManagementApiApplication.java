6b7ed48ad91fb45fe31b9cda81824a8346b21a04
Tran Kien Phong <kienphongtran2003@gmail.com>
2025-08-19 16:27:05 +0700

    config docker compose and docker file

--- src/main/java/com/example/managementapi/ManagementApiApplication.java
+++ src/main/java/com/example/managementapi/ManagementApiApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ManagementApiApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+//        Dotenv dotenv = Dotenv.configure().load();
+//        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(ManagementApiApplication.class, args);
     }
 
