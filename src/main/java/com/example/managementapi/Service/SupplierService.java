60867d613709dd7092bf44cd2048c6a3357bbe97
Tran Kien Phong <kienphongtran2003@gmail.com>
2025-08-23 21:00:21 +0700

    config Cloudinary -- adding Img for Create / Update Supplier

--- src/main/java/com/example/managementapi/Service/SupplierService.java
+++ src/main/java/com/example/managementapi/Service/SupplierService.java
@@ -3,6 +3,7 @@ package com.example.managementapi.Service;
 
 import com.example.managementapi.Dto.Request.Supplier.CreateSupplierReq;
 import com.example.managementapi.Dto.Request.Supplier.UpdateSupplierReq;
+import com.example.managementapi.Dto.Response.Cloudinary.CloudinaryRes;
 import com.example.managementapi.Dto.Response.Supplier.CreateSupplierRes;
 import com.example.managementapi.Dto.Response.Supplier.GetSupplierDetailRes;
 import com.example.managementapi.Dto.Response.Supplier.GetSupplierRes;
@@ -10,10 +11,12 @@ import com.example.managementapi.Dto.Response.Supplier.UpdateSupplierRes;
 import com.example.managementapi.Entity.Supplier;
 import com.example.managementapi.Mapper.SupplierMapper;
 import com.example.managementapi.Repository.SupplierRepository;
+import com.example.managementapi.Util.FileUpLoadUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -27,6 +30,9 @@ public class SupplierService {
 
     @Autowired
     private final SupplierMapper supplierMapper;
+
+    @Autowired
+    private final CloudinaryService cloudinaryService;
 
     public List<GetSupplierRes> getSuppliers(){
         return supplierRepository.findAll().stream()
@@ -42,7 +48,19 @@ public class SupplierService {
 
     public CreateSupplierRes createSupplier(CreateSupplierReq request){
 
+        MultipartFile image = request.getSupplierImg();
+        String imgUrl = null;
+
+        if(image != null && !image.isEmpty()){
+            FileUpLoadUtil.assertAllowed(image, FileUpLoadUtil.IMAGE_PATTERN);
+            String fileName = image.getOriginalFilename();
+            CloudinaryRes cloudinaryRes = cloudinaryService.uploadFile(image, fileName);
+            imgUrl = cloudinaryRes.getUrl();
+        }
+
        var supplier = supplierMapper.toCreateSupplierReq(request);
+
+        supplier.setSupplierImg(imgUrl);
 
        supplier = supplierRepository.save(supplier);
 
@@ -53,11 +71,22 @@ public class SupplierService {
 
         log.warn("supplier_id_service: " + supplierId);
 
+        MultipartFile image = request.getSupplierImg();
+        String imgUrl = null;
 
         Supplier supplier = supplierRepository.findById(supplierId).orElseThrow(()
                 -> new RuntimeException("Supplier not found"));
 
+        if(image != null && !image.isEmpty()){
+            FileUpLoadUtil.assertAllowed(image, FileUpLoadUtil.IMAGE_PATTERN);
+            String fileName = image.getOriginalFilename();
+            CloudinaryRes cloudinaryRes = cloudinaryService.uploadFile(image, fileName);
+            imgUrl = cloudinaryRes.getUrl();
+        }
+
         supplierMapper.toUpdateSupplierReq(supplier, request);
+
+        supplier.setSupplierImg(imgUrl);
 
         return supplierMapper.toUpdateSupplierRes(supplierRepository.save(supplier));
 
